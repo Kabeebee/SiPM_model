e8b8548be9a45f4cb5e72699098410ce101f7713
Connor Robinson <Connorrobinson19@gmail.com>
2021-01-25 14:49:26 +0000

    Data reading

---
+++ Datareader.py
@@ -0,0 +1,25 @@
+import csv
+import numpy as np
+
+def reader(filename):
+
+    datlist = []
+    dial = "Space"
+    xVals = np.array([])
+    Voltage = np.array([])
+
+
+    csv.register_dialect("Space", delimiter=" ")
+
+    with open(filename, "r", newline='') as file:
+        data = csv.reader(file, dialect=dial)
+        for row in data:
+            datlist.append(row)
+
+        for index in range(0, len(datlist), 100):
+            if datlist[index][5] != "-nan":
+                xVals = np.append(xVals, float(datlist[index][2]) * 10**9)
+                Voltage = np.append(Voltage, float(datlist[index][5]) * 10**3)
+
+    return(xVals, Voltage)
+         
