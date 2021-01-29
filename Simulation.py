6a4e031aae1555996da8037b9d919e2cbea636dc
Connor Robinson <Connorrobinson19@gmail.com>
2021-01-29 14:40:07 +0000

    chnaged data file, shifted data reading into analysis

--- Simulation.py
+++ Simulation.py
@@ -10,32 +10,20 @@ NUM_SIMS = 1
 # read in and store the data from csv called 'londat.csf'
 xdata = np.array([])
 ydata = np.array([])
-
 xdata, ydata = dr.reader("londat.csv")
 
-#**************************************************************************************
-# h5py takes numpy arrays as input so first create arrays from the data lists
-print(xdata)
+
 
 #**************************************************************************************
 # make the data file and fill it with data : )
 
-dataFile = h5py.File('outputdata\data.h5', 'w')
+dataFile = h5py.File('data.h5', 'w')
 dataFile.create_dataset('xdata', data = xdata)
 dataFile.create_dataset('ydata', data = ydata)
 dataFile.close()
 
-#**************************************************************************************
-# open the file and read the data back out
+print(len(xdata))
+plt.plot(xdata[:1000], ydata[:1000])
+plt.show()
 
-readFile = h5py.File('outputdata\data.h5', 'r')
-readX = readFile.get('xdata')
-readY = readFile.get('ydata')
-readX = np.array(readX)
-readY = np.array(readY)
-readFile.close()
-
-print(readX)
-
-#**************************************************************************************
 
