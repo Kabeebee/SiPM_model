1c1c276cab26e44d72df88c75102d6698e83d2b9
Connor Robinson <Connorrobinson19@gmail.com>
2021-02-22 15:45:20 +0000

    realised ap was actually DCT

--- Analysis.py
+++ Analysis.py
@@ -46,80 +46,83 @@ def main():
     countAP = 0
     countCT = 0
 
+    allTimes = np.array([])
 
     # load in y-data sets sequentially 
     while DataLeft:
-        for i in tqdm(range(numsims)):
+        
+        pulseData = Pulse()
 
-            pulseData = Pulse()
-
-            readFile = h5py.File('data.h5', 'r')
-            data = readFile.get("SPADPulse%d" % counter)
+        readFile = h5py.File('data.h5', 'r')
+        data = readFile.get("SPADPulse%d" % counter)
 
         # if there is no data stored under that name, we are out of data. stop the loop
-            if data == None:
-                DataLeft = False
-                break
-        # if data is [0] means that data is simply reference data + noise (which we will add here)
-            elif np.size(data) == 1:
-                refdat = readFile.get("referenceData")
-                data = np.array(refdat)
-                data += rand.normal(0, 1, len(data))
-                doTrue = False
-                readFile.close()
+        if data == None:
+            DataLeft = False
+            break
+    # if data is [0] means that data is simply reference data + noise (which we will add here)
+        elif np.size(data) == 1:
+            refdat = readFile.get("referenceData")
+            data = np.array(refdat)
+            data += rand.normal(0, 1, len(data))
+            doTrue = False
+            readFile.close()
 
-        # there is data here... analysis time
-            else:
-                TruthAP = readFile.get("APData%d" % counter)
-                TruthCT = readFile.get("CTData%d" % counter)
-                TruthAP = np.array(TruthAP)
-                TruthCT = np.array(TruthCT)
-                data = np.array(data)
-                doTrue = True
-                readFile.close()
+    # there is data here... analysis time
+        else:
+            TruthAP = readFile.get("APData%d" % counter)
+            TruthCT = readFile.get("CTData%d" % counter)
+            TruthAP = np.array(TruthAP)
+            TruthCT = np.array(TruthCT)
+            data = np.array(data)
+            doTrue = True
+            readFile.close()
 
-            # try and collect initial guess values to make curve fit easier
+        # try and collect initial guess values to make curve fit easier
 
-            numPeaks, peakPositions = calculate_num_peaks(data, template_pulse)
+        numPeaks, peakPositions = calculate_num_peaks(data, template_pulse)
 
-            for i in range(len(peakPositions)):
-                if i != 0:
-                    #print(peakPositions[i])
-                    pulseData.afterPulseTimes.append(peakPositions[i])
-                    amplitude = data[peakPositions[i]]
-                    pulseData.afterPulseAmplitudes.append(amplitude)
-                elif i == 0:
-                    
-                    amplitude = data[peakPositions[i]]
-                    CT = calculate_promptCT(amplitude, template_pulse)
-                    pulseData.numPromptCT = CT
-                    
-            pulseData.numAfterPulses = numPeaks - 1
-            if pulseData.numAfterPulses != 0:
-                print(f"{pulseData.numAfterPulses}/{TruthAP[0]}")
+        for i in range(len(peakPositions)):
+            if i != 0:
+                #print(peakPositions[i])
+                pulseData.afterPulseTimes.append(peakPositions[i])
+                amplitude = data[peakPositions[i]]
+                pulseData.afterPulseAmplitudes.append(amplitude)
+            elif i == 0:
+                
+                amplitude = data[peakPositions[i]]
+                CT = calculate_promptCT(amplitude, template_pulse)
+                pulseData.numPromptCT = CT
+                
+        pulseData.numAfterPulses = numPeaks - 1
+        if pulseData.numAfterPulses != 0:
+            try:
+                print(f"{pulseData.afterPulseTimes[0]}/{TruthCT[2]}")
+            except:
+                i = i
+            allTimes = np.append(allTimes, pulseData.afterPulseTimes[0])
+        # curve fit time (cross fingers)
 
-            # curve fit time (cross fingers)
+        initguess = (pulseData.numPromptCT, #amplitude
+                    4.0,                   #onset time
+                    1.0,                   #rise time
+                    5.0,                   #sharp decay time
+                    1000.0)                #long decay time
 
-            initguess = (pulseData.numPromptCT, #amplitude
-                        4.0,                   #onset time
-                        1.0,                   #rise time
-                        5.0,                   #sharp decay time
-                        1000.0)                #long decay time
-
-           # fitParams, fitCovariances = curve_fit(pulseFitFunc, xdata, data, p0 = initguess)
-
-
-            if doTrue == True:
-                trueAP += TruthAP[0]
-                trueCT += TruthCT[0]
-            countAP += pulseData.numAfterPulses
-            countCT += pulseData.numPromptCT
+        # fitParams, fitCovariances = curve_fit(pulseFitFunc, xdata, data, p0 = initguess)
 
 
-            counter += 1
+        if doTrue == True:
+            trueAP += TruthAP[0]
+            trueCT += TruthCT[0]
+        countAP += pulseData.numAfterPulses
+        countCT += pulseData.numPromptCT
+
+        counter += 1
 
     print(f"Found AP: {countAP}/{trueAP}")
     print(f"Found CT: {countCT}/{trueCT}")
+    print(allTimes)
 
 
 def calculate_num_peaks(data, template):
