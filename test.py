e3a77039271d47f445744990d8f2ddac70afea6b
Connor Robinson <Connorrobinson19@gmail.com>
2021-03-01 15:16:39 +0000

    made changes to peak fit

--- test.py
+++ test.py
@@ -50,7 +50,7 @@ def plotpulse(xval,yval,pulse1,pulse2):
     axis1 = fig.add_axes([0.12, 0.1, 0.85, 0.85]) # main axes
     axis1.plot(xval, yval, 'r-')
     axis1.plot(xval, pulse1, 'b-')
-    axis1.plot(xval, pulse2, 'g-')
+    #axis1.plot(xval, pulse2, 'g-')
     axis1.set_title('Dark Matter detector pulse', size=12)
     axis1.set_xlabel('Time [ns]', size=12)
     axis1.set_ylabel('Bias [mV]', size=12)
@@ -61,44 +61,41 @@ def plotpulse(xval,yval,pulse1,pulse2):
 
 readFile = h5py.File('data.h5', 'r')
 t = readFile.get("xdata")
-pulseData22 = readFile.get("SPADPulse6")
+pulseData22 = readFile.get("referenceData")
 pulseData22 = np.array(pulseData22)
 t = np.array(t)
 
 
-scale = 169.19
-onset = 0.876
-taurise = 3.35
-taushort = 3.957
-taulong = 10.92
-a = -2.27
-b = 23.88
-c = 3.175
-d = 1.0
+scale = 123.47
+onset = 1.218
+taurise = 1.506
+taushort = 10.577
+taulong = 10.590
+b = 6.08
+c = 15.42
 
-scale2 = 173.59
-onset2 = 99.00
-taurise2 = 3.801
-taushort2 = 4.621
-taulong2 = 10.825
-a2 = 385.49
-b2 = 411.396
-c2 =3.909
-d2 =1
 
-temp1  = np.exp(-(t - (onset + taurise)) / taushort)
-temp2  = np.exp(-(t - (onset + taurise)) / taulong)
+scale2 = 224
+onset2 = 95.1
+taurise2 = 4.11
+taushort2 = 5.233
+taulong2 = 11.175
+b2 = -4.105
+c2 =1.016
+
+temp1  = np.exp(-(t - (onset)) / taushort)
+temp2  = np.exp(-(t - (onset)) / taulong)
 decay = temp1 + temp2
-pulse = -scale * (np.exp(-(t - (onset + taurise)) / taurise) - decay)
-pulse[np.where(t<onset+taurise)] = -(c * t[np.where(t<onset+taurise)]-onset)*(d * t[np.where(t<onset+taurise)]-b)
-pulse[np.where(t < onset)] = 0.0
+pulse = -scale * (np.exp(-(t - (onset)) / taurise) - decay)
+#pulse[np.where(t<=onset+taurise)] = -(c * t[np.where(t<=onset+taurise)]-onset)*(t[np.where(t<=onset+taurise)] - onset -b)
+pulse[np.where(t < onset + taurise)] = 0.0
 
-temp3  = np.exp(-(t - onset2) / taushort2)
-temp4  = np.exp(-(t - onset2) / taulong2)
+temp3  = np.exp(-(t - (onset2 + taurise2)) / taushort2)
+temp4  = np.exp(-(t - (onset2 + taurise2)) / taulong2)
 decay2 = temp3 + temp4
-pulse2 = -scale2 * (np.exp(-(t - onset2) / taurise2) - decay2)
-pulse2[np.where(t<onset2+taurise2)] = -(c2 * t[np.where(t<onset2+taurise2)]-a2)*(d2 * t[np.where(t<onset2+taurise2)]-b2)
-pulse2[np.where(t < onset2)] = 0.0
+pulse2 = -scale2 * (np.exp(-(t - (onset2 + taurise2)) / taurise2) - decay2)
+#pulse2[np.where(t<=onset2+taurise2)] = -(c2 * t[np.where(t<=onset2+taurise2)]-onset)*(t[np.where(t<=onset2+taurise2)] - onset -b2)
+pulse2[np.where(t < onset2 + taurise)] = 0.0
 
 
 
